fea045ba737652116f0b28a1e6167f1fe6cbe56f
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2026-05-13 13:10:31 -0400

    Initial Commit

---
+++ BulletStormGameSpriteKit/BulletStormGameSpriteKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BulletStormGameSpriteKitApp.swift
+//  BulletStormGameSpriteKit
+//
+//  Created by Lidiia Diachkovskaia on 5/13/26.
+//
+
+import SwiftUI
+
+@main
+struct BulletStormGameSpriteKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BulletStormGameSpriteKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BulletStormGameSpriteKit
+//
+//  Created by Lidiia Diachkovskaia on 5/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
